cd94e666ffb50c7482c181a98a56f415f7075d7b
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2022-01-03 14:13:17 -0300

    Criar classe UsuarioController

--- src/main/java/br/com/zup/leadCollector/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/leadCollector/usuario/UsuarioController.java
@@ -1,4 +1,20 @@
 package br.com.zup.leadCollector.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping()
+    public Usuario cadastrarUsuario(@RequestBody Usuario usuario){
+        return usuarioService.salvarUsuario(usuario);
+    }
 }
--- src/main/java/br/com/zup/leadCollector/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/leadCollector/usuario/UsuarioService.java
@@ -1,8 +1,16 @@
 package br.com.zup.leadCollector.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvarUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
 }
