5f24c07a79c95cd8c8391f30a5f5819fbb5a58ef
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2022-01-04 10:47:05 -0300

    Criar classe ConfiguracaoDeSeguranca

---
+++ src/main/java/br/com/zup/leadCollector/config/security/ConfiguracaoDeSeguranca.java
@@ -0,0 +1,11 @@
+package br.com.zup.leadCollector.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+}
