ce7783a63c4856980afb2dedc3b5fbb7610010b2
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2022-01-04 10:54:04 -0300

    Criar metodo de acesso ao metodo GET no endpoint /leads

--- src/main/java/br/com/zup/leadCollector/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/leadCollector/config/security/ConfiguracaoDeSeguranca.java
@@ -1,6 +1,8 @@
 package br.com.zup.leadCollector.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/leads")
+                .permitAll().anyRequest().authenticated();
+    }
 }
