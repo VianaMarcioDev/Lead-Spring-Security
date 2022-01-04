733c6b65518c811b2b02e1e06988e2d5f01a271c
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2022-01-04 11:28:15 -0300

    Criar @Bean para configurar CORS

--- src/main/java/br/com/zup/leadCollector/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/leadCollector/config/security/ConfiguracaoDeSeguranca.java
@@ -1,10 +1,14 @@
 package br.com.zup.leadCollector.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -20,9 +24,17 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
+        http.cors().configurationSource(configurarCORS());
 
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO)
                 .permitAll().anyRequest().authenticated();
+    }
+
+    @Bean
+    CorsConfigurationSource configurarCORS(){
+        UrlBasedCorsConfigurationSource cors = new UrlBasedCorsConfigurationSource();
+        cors.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return cors;
     }
 }
